f4eb58de41f1fd3de62e2d44e0aa0cd8d487c728
jeremywhite96 <89687533+jeremywhite96@users.noreply.github.com>
2022-11-30 17:00:26 +0000

    30/11

--- components/bubble/BubSketches.js
+++ components/bubble/BubSketches.js
@@ -1,5 +1,37 @@
+import Masonry from 'react-masonry-css'
+import Image from 'next/image';
+
 const BubSketches = () => {
-    return (  );
+    return ( 
+        <Masonry breakpointCols={3} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                <Image src='/static/images/bubble/IA/proactive.png' className="object-contain h-max w-full" width="750" height="1000"/>
+                
+        </Masonry>
+     );
 }
  
 export default BubSketches;
