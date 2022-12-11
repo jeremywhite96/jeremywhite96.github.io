c698f51ed63d50781aba9b11bedaea22304feccf
jeremywhite96 <89687533+jeremywhite96@users.noreply.github.com>
2022-12-11 18:48:44 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -76,6 +76,12 @@ module.exports = withBundleAnalyzer({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // fixes next-mdx-remote: Package path ./jsx-runtime.js is not exported from package react
+      // https://github.com/hashicorp/next-mdx-remote/issues/237
+      "react/jsx-runtime.js": require.resolve("react/jsx-runtime"),
+    };
 
     if (!dev && !isServer) {
       // Replace React with Preact only in client production build
